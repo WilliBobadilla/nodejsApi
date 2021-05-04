1ef09a4462d252a047ec74d4ebc916ddc25ffa8c
WilliBobadilla <willi1997.1@gmail.com>
2021-05-03 23:21:45 -0400

    adding user models

---
+++ models/user.js
@@ -0,0 +1,32 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    idNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    direction: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
